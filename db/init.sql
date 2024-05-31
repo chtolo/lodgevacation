02d30b4357401cb0db6824496a33c7d85381800e
salma MOTTAKI <135275877+chtolo@users.noreply.github.com>
2024-06-01 00:57:02 +0200

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -4,7 +4,7 @@ USE mydb;
 CREATE TABLE IF NOT EXISTS user (
     userid INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
@@ -19,5 +19,11 @@ CREATE TABLE IF NOT EXISTS reservation (
 );
 
 -- Insérez quelques données de test
-INSERT INTO user (name, email, password) VALUES ('salma', 'salma@email.com', 'password123'), ('Bob', 'bob@example.com', 'password456');
-INSERT INTO reservation (email, nom_maison, date, nombre_personnes, nombre_nuits) VALUES ('alice@example.com', 'Maison de Alice', '2024-06-01', 4, 3), ('bob@example.com', 'Maison de Bob', '2024-06-10', 2, 2);
+INSERT INTO user (name, email, password) VALUES 
+('salma', 'salma@email.com', 'password123'), 
+('bob', 'bob@example.com', 'password456');
+
+INSERT INTO reservation (email, nom_maison, date, nombre_personnes, nombre_nuits) 
+VALUES 
+('salma@email.com', 'Maison de Salma', '2024-06-01', 4, 3),
+('bob@example.com', 'Maison de Bob', '2024-06-10', 2, 2);
